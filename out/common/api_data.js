54d71f2cd9b7740dea468e95900997624cbec61e
Dsc <546328100@qq.com>
2018-10-11 09:11:41 +0800

    更新

--- out/common/api_data.js
+++ out/common/api_data.js
@@ -1,5 +1,61 @@
 define({ "api": [
   {
+    "type": "get",
+    "url": "/baidu/speech",
+    "title": "语音合成",
+    "name": "speech",
+    "group": "baidu",
+    "version": "1.0.0",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "text",
+            "description": "<p>文字 {query}</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": true,
+            "field": "spd",
+            "description": "<p>语速，取值0-9，默认为5中语速 {query}</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": true,
+            "field": "pit",
+            "description": "<p>音调，取值0-9，默认为5中语调 {query}</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": true,
+            "field": "vol",
+            "description": "<p>音量，取值0-15，默认为5中音量 {query}</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": true,
+            "field": "per",
+            "description": "<p>发音人选择, 0为女声，1为男声，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女 {query}</p>"
+          }
+        ]
+      }
+    },
+    "filename": "app/controller/common/baidu.js",
+    "groupTitle": "baidu",
+    "sampleRequest": [
+      {
+        "url": "http://kyun.dusuchao.xin:7001/common/v1/baidu/speech"
+      }
+    ]
+  },
+  {
     "type": "post",
     "url": "/captcha/email",
     "title": "发送邮箱，找回密码",
@@ -182,6 +238,61 @@ define({ "api": [
     "sampleRequest": [
       {
         "url": "http://kyun.dusuchao.xin:7001/common/v1/captcha/verify"
+      }
+    ]
+  },
+  {
+    "type": "post",
+    "url": "/google/speech",
+    "title": "语音合成",
+    "name": "speech",
+    "group": "google",
+    "version": "1.0.0",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "token",
+            "description": "<p>令牌 {header}</p>"
+          }
+        ]
+      }
+    },
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "text",
+            "description": "<p>文字 {form}</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": true,
+            "field": "lang",
+            "description": "<p>语言，默认英文en。中文zh {form}</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": true,
+            "field": "speed",
+            "description": "<p>语速，默认1 {form}</p>"
+          }
+        ]
+      }
+    },
+    "filename": "app/controller/common/google.js",
+    "groupTitle": "google",
+    "sampleRequest": [
+      {
+        "url": "http://kyun.dusuchao.xin:7001/common/v1/google/speech"
       }
     ]
   }
