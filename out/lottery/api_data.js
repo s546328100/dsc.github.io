94a584d2e4799e3d2e21f19da6f080a6e845ef17
Dsc <546328100@qq.com>
2019-01-17 10:00:43 +0800

    doc

--- out/lottery/api_data.js
+++ out/lottery/api_data.js
@@ -1,7 +1,49 @@
 define({ "api": [
   {
     "type": "post",
-    "url": "/lottery/user/login",
+    "url": "/v1/activity",
+    "title": "用户授权登陆",
+    "name": "create",
+    "group": "activity",
+    "version": "1.0.0",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "code",
+            "description": ""
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "encryptedData",
+            "description": ""
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "iv",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "filename": "src/controller/v1/activity.ts",
+    "groupTitle": "activity",
+    "sampleRequest": [
+      {
+        "url": "http://192.168.31.219:5761/lottery/v1/activity"
+      }
+    ]
+  },
+  {
+    "type": "post",
+    "url": "/v1/user/login",
     "title": "用户授权登陆",
     "name": "userLogin",
     "group": "user",
@@ -33,11 +75,11 @@ define({ "api": [
         ]
       }
     },
-    "filename": "src/controller/lottery/user.ts",
+    "filename": "src/controller/v1/user.ts",
     "groupTitle": "user",
     "sampleRequest": [
       {
-        "url": "http://192.168.31.219:5761/weapp/lottery/user/login"
+        "url": "http://192.168.31.219:5761/lottery/v1/user/login"
       }
     ]
   }
--- out/lottery/api_project.js
+++ out/lottery/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "1.0.0",
   "description": "错误格式 {code: 500, msg: '失败', success: false} code为数字且大于0，msg为错误信息",
   "title": "抽奖接口文档",
-  "url": "http://192.168.31.219:5761/weapp",
-  "sampleUrl": "http://192.168.31.219:5761/weapp",
+  "url": "http://192.168.31.219:5761/lottery",
+  "sampleUrl": "http://192.168.31.219:5761/lottery",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-16T10:06:26.930Z",
+    "time": "2019-01-17T02:00:17.934Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
