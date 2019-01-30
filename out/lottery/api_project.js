263870d86924c0ebb652da2ea16bf70d052cf655
Dsc <546328100@qq.com>
2019-01-30 17:25:46 +0800

    doc

--- out/lottery/api_project.js
+++ out/lottery/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-30T07:26:43.733Z",
+    "time": "2019-01-30T09:25:35.004Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
