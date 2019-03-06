28d586d63d52f8d2500966272da23013ba5bfa86
Dsc <546328100@qq.com>
2019-03-06 15:58:22 +0800

    out

---
+++ out/shop/admin/api_data.js
@@ -0,0 +1,86 @@
+define({ "api": [
+  {
+    "type": "post",
+    "url": "/admin/coupon",
+    "title": "创建优惠劵",
+    "name": "adminPostCoupon",
+    "group": "coupon",
+    "version": "1.0.0",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "token",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "name",
+            "description": "<p>名称</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "money",
+            "description": "<p>金额</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "status",
+            "description": "<p>状态 0下架 1上架</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "num",
+            "description": "<p>数量</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": true,
+            "field": "moneyOff",
+            "defaultValue": "0",
+            "description": "<p>满xx元使用</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": true,
+            "field": "startTime",
+            "description": "<p>能开始领取/使用时间</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": true,
+            "field": "endTime",
+            "description": "<p>失效时间</p>"
+          }
+        ]
+      }
+    },
+    "filename": "src/controller/admin/coupon.ts",
+    "groupTitle": "coupon",
+    "sampleRequest": [
+      {
+        "url": "http://192.168.31.219:5762/shop/admin/coupon"
+      }
+    ]
+  }
+] });
---
+++ out/shop/v1/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "shop",
+  "version": "1.0.0",
+  "description": "错误格式 {code: 500, msg: '失败', success: false} code为数字且大于0，msg为错误信息",
+  "title": "商城接口文档",
+  "url": "http://192.168.31.219:5762/shop",
+  "sampleUrl": "http://192.168.31.219:5762/shop",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-06T07:57:51.411Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
