bfb6611b35e8b1dae6cc6f62584f25e648414164
Dsc <546328100@qq.com>
2019-03-12 10:08:59 +0800

    doc

--- out/shop/admin/api_project.js
+++ out/shop/admin/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-03-08T03:46:42.600Z",
+    "time": "2019-03-12T02:08:14.090Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
