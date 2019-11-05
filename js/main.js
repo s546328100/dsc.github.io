39fba08f9d8adfb314abfb0d0e5aa4786adb3ce6
dsc <546328100@qq.com>
2019-11-05 10:21:13 +0800

    images

--- js/main.js
+++ js/main.js
@@ -1,126 +1,15 @@
-(function() {
-    let contentWayPoint = function() {
-        let i = 0;
-        $('.animate-box').waypoint(
-            //第1个参数为waypoints事件响应时所执行的代码，是一个匿名函数即可
-            function(direction) {
-                if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
-                    i++;
-                    $(this.element).addClass('item-animate');
-                    setTimeout(() => {
-                        $('body .animate-box.item-animate').each(function(k) {
-                            var el = $(this);
-                            setTimeout(
-                                () => {
-                                    el.addClass('fadeInUp animated-fast');
+const s = require('events');
 
-                                    el.removeClass('item-animate');
-                                },
-                                k * 200,
-                                'easeInOutExpo'
-                            );
-                        });
-                    }, 100);
-                }
-            },
-            //第2个参数为偏移量,本例即该div到窗口高度90%时触发
-            {offset: '90%'}
-        );
-    };
-
-    let goToTop = function() {
-        $('html, body').animate(
-            {
-                scrollTop: $('html').offset().top
-            },
-            500,
-            'easeInOutExpo'
-        );
-    };
-
-    let clickToTop = function() {
-        $('.js-gotop').on('click', function(event) {
-            event.preventDefault();
-            goToTop();
-            return false;
-        });
-
-        $(window).scroll(debounce(handle, 100));
-        // 防抖
-        function debounce(fn, wait) {
-            var timeout = null;
-            return function() {
-                if (timeout !== null) clearTimeout(timeout);
-                timeout = setTimeout(fn, wait);
-            };
-        }
-        // 处理函数
-        function handle() {
-            let currentScroll = $(window).scrollTop();
-
-            // 回顶部
-            top(currentScroll);
-
-            // 文章目录
-            section(currentScroll);
-
-            lazyload();
-        }
-
-        function top(currentScroll) {
-            if (currentScroll > 200) $('.gototop').addClass('active');
-            else $('.gototop').removeClass('active');
-        }
-
-        function section(currentScroll) {
-            let currentSection;
-            $(':header').each(function(i, v) {
-                let divPosition = $(this).offset().top;
-                if (divPosition - 1 < currentScroll) {
-                    $currentSection = $(this);
-                }
-
-                if (currentScroll > 100) {
-                    let id = $currentSection.attr('id');
-                    $('a[href*=\\#]').removeClass('card-activate');
-                    $('a[href=\\#' + id).addClass('card-activate');
-                }
-            });
-
-            let comment = $('#comment').offset() || '';
-            if (comment) {
-                if (comment.top - currentScroll <= 300) {
-                    $('a[href*=\\#]').removeClass('card-activate');
-                    $('a[href=\\#' + 'comment').addClass('card-activate');
-                }
-            }
-        }
-    };
-
-    let lazyload = function() {
-        let currentScroll = $(window).scrollTop();
-
-        let lazyImgs = $.map($('img[data-src]').get(), function(i) {
-            return $(i);
-        });
-
-        console.log(lazyImgs);
-        if (lazyImgs.length <= 0) return;
-
-        let wheight = $(window).height();
-        $.each(lazyImgs, function(index, $i) {
-            if ($i.offset().top - currentScroll - 300 < wheight) {
-                $i.attr('src', $i.attr('data-src'));
-                $i.addClass('lazy-show');
-                $i.removeAttr('data-src');
-            }
-        });
-    }
-
-    $(function() {
-        contentWayPoint();
-        clickToTop();
-        goToTop();
-        lazyload();
-    });
-})();
+class EE extends s.EventEmitter {}
+const yy = new EE()
+yy.on('event', () => console.log('粗大事啦'))
+setTimeout(() => console.log('0 毫秒后到期的定时器回调'), 0)
+setTimeout(() => console.log('100 毫秒后到期的定时器回调'), 100)
+setImmediate(() => console.log('immediate 立即回调'))
+process.nextTick(() => console.log('process.nextTick 的回调1'))
+Promise.resolve().then(() => {
+  yy.emit('event')
+  process.nextTick(() => console.log('process.nextTick 的回调2'))
+  console.log('promise 第一次回调')
+})
+.then(() => console.log('promise 第二次回调'))
